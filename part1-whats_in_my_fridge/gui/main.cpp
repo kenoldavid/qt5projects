4d47778a7ca29443dfa2b182ab11726dcb883ee3
Kenol David <kenoldavid.git@gmail.com>
2023-09-10 22:19:42 -0400

    Set application name and QQuickStyle methods

--- part1-whats_in_my_fridge/gui/main.cpp
+++ part1-whats_in_my_fridge/gui/main.cpp
@@ -1,11 +1,15 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickStyle>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QCoreApplication::setApplicationName("splashwindow demo");
+    //    QCoreApplication::setApplicationName("What's in my fridge?");
     QCoreApplication::setApplicationVersion("v1.0.0");
+    // QQuickStyle::setStyle("Material");
+    // QQuickStyle::setFallbackStyle("Material");
 
     QGuiApplication app(argc, argv);
 
@@ -13,6 +17,8 @@ int main(int argc, char *argv[])
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
+
+    qDebug() << "Style(s) available: " << QQuickStyle::name();
 
     return app.exec();
 }
